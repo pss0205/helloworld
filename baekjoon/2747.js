3cb6bd608f05faad94404f5001ea2abba453a0f5
Your Name <pss070205@gmail.com>
2021-11-27 21:27:28 +0900

    update for 2747.js

--- baekjoon/2747.js
+++ baekjoon/2747.js
@@ -1,14 +1,17 @@
 const fs = require('fs');
 const n = parseInt(fs.readFileSync('/dev/stdin').toString());
-
-let curr = 0;
-let next = 1;
-let temp;
-for(let i = 0; i < n; i++)
+function fibo(n)
 {
-    temp = curr;
-    curr = next;
-    next = next + temp;
+    let curr = 0;
+    let next = 1;
+    let temp;
+    for(let i = 0; i < n; i++)
+    {
+        temp = curr;
+        curr = next;
+        next = next + temp;
+    }
+    return curr;
+    
 }
-console.log(curr);
-
+console.log(fibo(n));
